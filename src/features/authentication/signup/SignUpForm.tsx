cce105330250e3f19ab475b9613dda4bd8685a51
신승훈 <zlzlzlmo@daum.net>
2022-07-21 22:28:04 +0900

    add signup custom hook

--- src/features/authentication/signup/SignUpForm.tsx
+++ src/features/authentication/signup/SignUpForm.tsx
@@ -3,16 +3,39 @@ import React from "react";
 import Input, { OInputType } from "../../../components/atoms/input";
 import Button from "../components/Button";
 import Label from "../components/Label";
+import useSignUpForm from "./hooks/useSignUpForm";
 
 const SignUpForm = () => {
+  const {
+    signUpInfo,
+    isValidSignUp,
+    handleEmail,
+    handlePassword,
+    handleUserName,
+  } = useSignUpForm();
   return (
     <View style={styles.container}>
-      <Input inputType={OInputType.email} placeholder="Email" />
-      <Input inputType={OInputType.normal} placeholder="UserName" />
-      <Input inputType={OInputType.secure} placeholder="Password" />
+      <Input
+        inputType={OInputType.email}
+        placeholder="Email"
+        value={signUpInfo.email}
+        onChangeText={handleEmail}
+      />
+      <Input
+        inputType={OInputType.normal}
+        placeholder="UserName"
+        value={signUpInfo.userName}
+        onChangeText={handleUserName}
+      />
+      <Input
+        inputType={OInputType.secure}
+        placeholder="Password"
+        value={signUpInfo.password}
+        onChangeText={handlePassword}
+      />
 
       <View style={styles.buttonContainer}>
-        <Button active={false} onPress={() => {}}>
+        <Button active={isValidSignUp} onPress={() => {}}>
           Sign Up
         </Button>
       </View>
---
+++ src/features/authentication/signup/hooks/useSignUpForm.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useMemo, useState } from "react";
+import ValidatorFactory from "../../../../util/validator/validatorFactory";
+
+type SignUpInfoKeyType = "email" | "password" | "userName";
+
+const useSignUpForm = () => {
+  const [signUpInfo, setSignUpInfo] = useState<
+    Record<SignUpInfoKeyType, string>
+  >({
+    email: "",
+    userName: "",
+    password: "",
+  });
+
+  const isValidSignUp = useMemo<boolean>(() => {
+    const { email, userName, password } = signUpInfo;
+    const result =
+      ValidatorFactory.createValidator("email", email).isValid() &&
+      ValidatorFactory.createValidator("userName", userName).isValid() &&
+      ValidatorFactory.createValidator("password", password).isValid();
+
+    return result;
+  }, [signUpInfo]);
+
+  const handleEmail = (email: string) => {
+    setSignUpInfo((signUpInfo) => ({ ...signUpInfo, email }));
+  };
+
+  const handleUserName = (userName: string) => {
+    setSignUpInfo((signUpInfo) => ({ ...signUpInfo, userName }));
+  };
+
+  const handlePassword = (password: string) => {
+    setSignUpInfo((signUpInfo) => ({ ...signUpInfo, password }));
+  };
+
+  return {
+    signUpInfo,
+    isValidSignUp,
+    handleEmail,
+    handleUserName,
+    handlePassword,
+  };
+};
+
+export default useSignUpForm;
